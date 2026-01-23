df9146ca88c1663e572a246bd68429b948375560
Matt Gross <50339599+CinnamonGrossCrunch@users.noreply.github.com>
2026-01-23 12:11:06 -0800

    Remove archived-old-root-code folder - unused duplicate code causing build issues

--- archived-old-root-code/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import ClientDashboard from "./components/ClientDashboard";
-
-export default function Home() {
-  // Client-side fetching - data will be pre-cached by cron jobs
-  return <ClientDashboard initialData={null} />;
-}
